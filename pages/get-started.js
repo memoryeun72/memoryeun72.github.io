01425c7fc56c2da536edab8d9e87c9eb8f305e96
memoryeun72 <memoryeun72@douzone.com>
2019-09-01 14:56:26 +0900

    first

---
+++ pages/get-started.js
@@ -0,0 +1,12 @@
+import Nav from '../components/nav';
+
+export default () => {
+    return ( <>
+        <Nav/>
+
+        <h1>시작하기</h1>
+
+        <p>npm을 통해 CLI를 설치합니다. </p>
+        </>
+    )
+}
